d6cf40d073788010fdc6c26bfcc83dc7bc7ef284
Arex23 <garbuja12alson@gmail.com>
2019-08-03 14:55:22 +0545

    fixed the time_adder code

--- lab-3.2/time_adder_with_func_havingobjasparameter.cpp
+++ lab-3.2/time_adder_with_func_havingobjasparameter.cpp
@@ -15,12 +15,13 @@ time_adder time_adder :: display(time_adder t1){
 	
 	time_adder t3;
 	
-		t3.s = t1.s + t2.s;
-		t3.m = t1.m + t2.m + t3.s/60;
+		t3.s = t1.s + s;
+		t3.m = t1.m + m + t3.s/60;
 		t3.s %= 60;
-		t3.h = t1.h + t2.h + t3.m/60;
+		t3.h = t1.h + h + t3.m/60;
 		t3.m %= 60;			
 			cout << "Time: " << t3.h << ":" << t3.m << ":" << t3.s;
+			
 }
 
 main(){
